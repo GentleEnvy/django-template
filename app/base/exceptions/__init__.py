f0989d178c6556596352436568ee8f2418d1bb39
GentleEnvy <komarov.sergei163@gmail.com>
2022-05-31 08:38:06 +0300

    add dto

--- app/base/exceptions/__init__.py
+++ app/base/exceptions/__init__.py
@@ -1,3 +1,4 @@
 from .client import *
 from .critical import *
 from .warning import *
+from .entity import *
--- app/users/controllers/password.py
+++ app/users/controllers/password.py
@@ -1,10 +1,11 @@
 import dataclasses
 
-from dataclass_type_validator import dataclass_validate, TypeValidationError
 from django.conf import settings
 from templated_mail.mail import BaseEmailMessage
 
 from app.base.controllers.base import BaseController
+from app.base.entities.base import BaseEntity
+from app.base.exceptions import EntityValidationError
 from app.users.models import User
 from app.users.services.auth import AuthService
 from app.users.services.email_verification import EmailVerificationService
@@ -18,19 +19,18 @@ class GET_UsersPasswordController(BaseController):
     email_verification: EmailVerificationService = {'scope': 'password'}
     password_session: PasswordSessionService
     
-    @dataclass_validate
     @dataclasses.dataclass
-    class _dataclass:
+    class _dataclass(BaseEntity):
         email: str
         code: str
     
-    def dataclass(self) -> _dataclass | None:
+    def dto(self) -> _dataclass | None:
         query_params = self.view.request.query_params
         try:
             return self._dataclass(
                 email=query_params.get('email'), code=query_params.get('code')
             )
-        except (TypeError, TypeValidationError):
+        except (TypeError, EntityValidationError):
             return None
     
     def control(self, data: _dataclass | None):
@@ -43,12 +43,11 @@ class GET_UsersPasswordController(BaseController):
 class POST_UsersPasswordController(BaseController):
     email_verification: EmailVerificationService = {'scope': 'password'}
     
-    @dataclass_validate
     @dataclasses.dataclass
-    class dataclass:
+    class dto(BaseEntity):
         email: str
     
-    def control(self, data: dataclass):
+    def control(self, data: dto):
         self.email_verification.send(
             BaseEmailMessage(
                 request=self.view.request, template_name='users/password.html',
@@ -61,13 +60,12 @@ class PUT_UsersPasswordController(BaseController):
     email_verification: EmailVerificationService = {'scope': 'password'}
     password_session: PasswordSessionService
     
-    @dataclass_validate
     @dataclasses.dataclass
-    class dataclass:
+    class dto(BaseEntity):
         session_id: str
         password: str
     
-    def control(self, data: dataclass):
+    def control(self, data: dto):
         if (email := self.password_session.check(data.session_id)) is None:
             raise self.view.serializer.WARNINGS[408]
         user = User.objects.get(email=email)
