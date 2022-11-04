6931628d228c515b508f1cbe6b3cf63d02ec110b
GentleEnvy <komarov.sergei163@gmail.com>
2022-11-04 18:09:24 +0300

    fix black
    Signed-off-by: GentleEnvy <komarov.sergei163@gmail.com>

--- app/users/managers.py
+++ app/users/managers.py
@@ -22,5 +22,4 @@ class UserManager(_UserManager):
         extra_fields.setdefault('is_active', True)
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
-        return self._create_user(
-            email, password, first_name, **extra_fields)
+        return self._create_user(email, password, first_name, **extra_fields)
